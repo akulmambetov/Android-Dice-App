b7cd4073832a13b3519d44cc0a8e3e40fd5e5b0e
Atai Kulmambetov <atai.k@intelliants.com>
2024-10-15 19:21:23 +0600

    feat: add dice image

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dicerollerapp.ui.theme.DiceRollerAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,6 +43,11 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(R.drawable.dice_1),
+            contentDescription = "1"
+        )
+        Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = { /*TODO*/ }) {
             Text(stringResource(R.string.roll))
         }
